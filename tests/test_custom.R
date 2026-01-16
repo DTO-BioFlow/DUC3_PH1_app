f97ddf2511b4192f4066d76ce744b4b2245f0111
Willem Boone <willem.boone@vliz.be>
2026-01-16 14:14:14 +0100

    fixed costum module

---
+++ tests/test_custom.R
@@ -0,0 +1,19 @@
+library(here)
+
+# Ensure path is relative to project root
+source(here("modules/PH1_function.R"))
+
+df <- read.csv(
+  "https://raw.githubusercontent.com/DTO-BioFlow/DUC3_dataset_inventory/refs/heads/main/data_sets/EDITO_dasid_4687_SCHPM1_holo_mero.csv"
+)
+
+results <- run_ph1_analysis(
+  df = df,
+  ref_years  = c(2015, 2018),
+  comp_years = c(2018, 2025),
+  mon_thr    = 8
+)
+
+results$datasets$PI_results
+results$env_plots
+results$ts_plots
